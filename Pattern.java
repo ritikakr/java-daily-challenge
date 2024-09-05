67e9a74cc7f1acdc685ad5ce806759306e380e36
ritikakr <90204179+ritikakr@users.noreply.github.com>
2024-09-05 09:56:38 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,37 @@
+package com.basic;
+
+public class Pattern 
+{
+		public static void printPattern(int n)
+	    {
+	        int i, j;
+	        // outer loop to handle number of rows
+	        for (i = 0; i < n; i++) 
+	        {
+	           
+	            for (j = 0; j < n; j++)
+	            {
+	                
+	                if (i == 0 || j == 0 || i== n - 1 || j == n - 1)
+	                {
+	                    System.out.print("*");
+	                }
+	               else 
+	               {
+	                    System.out.print(" ");
+	               }
+	            }
+	            System.out.println();
+	             
+	            
+	    }
+	   }
+	public static void main(String args[])
+	    {
+	        int n = 6;
+	        printPattern(n);
+	    }
+
+}
+
+
