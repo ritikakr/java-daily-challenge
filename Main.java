89dcf2d741dfcdcd715291799f7134599079dd2b
ritikakr <90204179+ritikakr@users.noreply.github.com>
2024-09-25 15:16:35 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,85 @@
-package com.access;
+package com.day4;
 
 public class Main 
 {
-
-	public static void main(String[] args) 
+	public class Hillstation 
 	{
-		Circle c = new Circle();
-		c.area_circle(4);
-		
-		Rectangle r = new Rectangle();
-		r.area_rect(2.50, 4.50);
-					
+		public void location()
+		{
+			System.out.println("Hillstation.");
+		}
+				
+		public void famousfor()
+				
+		{
+			System.out.println("Famous for its weather and views.");
+		}
+
+	
 	}
 
-}
+	public class Manali extends Hillstation
+	{
+		public void location()
+		{
+			System.out.println("Manali");
+			
+		}
+		
+		public void famousfor()
+		{
+			System.out.println("Manali is famous for snow-capped mountains, adventure sports, and scenic beauty.");
+			super.location();
+			super.famousfor();
+		}
+	}
+	
+
+	public class Mussoorie extends Manali
+	{
+		public void location()
+		{
+			System.out.println("Mussoorie.");
+			
+		}
+		
+		public void famousfor()
+		{
+			System.out.println("Mussoorie is famous for its colonial architecture, scenic views, and pleasant weather.");
+			super.location();
+			super.famousfor();
+		}
+	}
+	
+
+	public class Gulmarg extends Mussoorie
+	{
+		public void location()
+		{
+			System.out.println("Gulmarg.");
+			
+		}
+		
+		public void famousfor()
+		{
+			System.out.println("Gulmarg is famous for its gondola rides, skiing, and meadows.");
+			super.location();
+			super.famousfor();
+		}
+
+	}
+	public static void main(String[] args)
+	{
+		Main m = new Main();
+		
+		Gulmarg gm = m.new Gulmarg();
+		
+		gm.location();
+		gm.famousfor();
+				
+	}
+		
+	}
+
+
+
